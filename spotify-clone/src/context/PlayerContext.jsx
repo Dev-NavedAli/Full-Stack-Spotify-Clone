395ec3840d792d524852836d54ac0df2234dc5bd
Naved khan <nomanali50997@gmail.com>
2024-10-06 19:35:46 +0530

    p

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext()
@@ -9,11 +9,49 @@ const PlayerConextProvider = (props) => {
     const seekBg = useRef()
     const seekBar = useRef()
 
-    const [track,setTrack] = useState(songsData[0])
-    const [playStatus,setPlayStatus] = useState(false)
+    const [ track , setTrack ] = useState( songsData[0])
+    const [ playStatus , setPlayStatus ] = useState(false)
+    const [ time , setTime ] = useState({
+        currentTime: {
+            second: 0,
+            minute: 0,
+        },
+        totalTime: {
+            second: 0,
+            minute: 0,
+        }
+    })
+
+    const play = ()=>{
+        audioRef.current.play()
+        setPlayStatus(true)
+    }
+
+    const pause = ()=>{
+        audioRef.current.pause()
+        setPlayStatus(false)
+    }
+
+
+    useEffect(()=>{
+       setTimeout(()=>{
+        audioRef.current.ontimeUpdate = ()=>{
+            setTime({
+                currentTime: {
+                    second: Math.floor(audioRef.current.currentTime % 60),
+                    minute: Math.floor(audioRef.current.currentTime / 60),
+                },
+                totalTime: {
+                    second: Math.floor(audioRef.current.duration % 60),
+                    minute: Math.floor(audioRef.current.duration / 60),
+                }
+            })
+        }
+       })
+    },[audioRef])
 
     const contextValue = {
-       audioRef,seekBar,seekBg
+        audioRef, seekBar, seekBg,track,setTrack,playStatus,setPlayStatus,time,setTime,play,pause
     }
     return (
         <PlayerContext.Provider value={contextValue}>
