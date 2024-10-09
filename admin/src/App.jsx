14c149321ab22aa4246e508f70731db306a0f269
Naved khan <nomanali50997@gmail.com>
2024-10-09 11:42:17 +0530

    making pages and components

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route } from 'react-router-dom';
+import AddAlbum from './pages/AddAlbum';
+import AddSong from './pages/AddSong';
+import ListAlbum from './pages/ListAlbum';
+import ListSong from './pages/ListSong';
+import SideBar from './components/SideBar';
 
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-red-300'>Naved</h1>
+    <div className='flex items-start min-h-screen '>
+      <ToastContainer />
+      <SideBar/>
+      <div className="flex-1 h-screen overflow-y-scroll bg-[#f3fff7]">
+        <div className="pt-8 pl-5 sm:pt-12 sm:pl-12">
+          <Routes>
+            <Route path='/add-song' element={<AddSong />} />
+            <Route path='/add-album' element={<AddAlbum />} />
+            <Route path='/list-song' element={<ListSong />} />
+            <Route path='/list-album' element={<ListAlbum />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   )
 }
