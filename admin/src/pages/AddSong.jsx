9b60c9e217774fab3e1da4cb73ccf80248b061d2
Naved khan <nomanali50997@gmail.com>
2024-10-11 12:22:22 +0530

    second final touch

--- admin/src/pages/AddSong.jsx
+++ admin/src/pages/AddSong.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import axios from "axios"
 import { url } from '../App'
@@ -45,6 +45,24 @@ const AddSong = () => {
     setLoading(false)
   }
 
+  const loadAlbumData = async()=>{
+    try {
+      const response = await axios.get(`${url}/api/album/list`)
+      if(response.data.success){
+        setAlbumData(response.data.allAlbum)
+      }else{
+        toast.error("Error Ocuured")
+      }
+    } catch (error) {
+      toast.error("Something went wrong")
+    }
+  }
+  
+  useEffect(()=>{
+    loadAlbumData()
+  },[])
+
+
   return loading ? (
     <div className='grid place-items-center min-h-[80vh]'>
       <div className="w-16 h-16 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin"></div>
@@ -78,8 +96,8 @@ const AddSong = () => {
       </div>
       <div className="flex flex-col gap-2.5">
         <p>Album </p>
-        <select onChange={(e) => setAlbum(e.target.value)} defaultValue={album} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px]' >
-          <option value="none">none</option>
+        <select onChange={(e) => setAlbum(e.target.value)} defaultValue={album} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px]' > 
+          {albumData.map((item,index)=>(<option key={index} value={item.name}>{item.name}</option>))}
         </select>
       </div>
       <button type='submit' className='text-base bg-black text-white py-2.5 px-14 cursor-pointer mb-8 rounded-lg'>Add</button>
